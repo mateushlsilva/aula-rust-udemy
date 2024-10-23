b91b8d5cfd0cf2a51a6cfe4f483b186b2d72e51a
mateushlsilva <mateushls01@gmail.com>
2024-10-23 09:10:18 -0300

    feature add enums

--- aula02/src/main.rs
+++ aula02/src/main.rs
@@ -24,6 +24,12 @@ enum CarType {
     Fiat, Ford, Renault
 }
 
+enum Pagamento {
+    Dinheiro(f32),
+    CreditoCartao(bool, f32),
+    DebitoCartao(bool, f32),
+}
+
 fn enums(){
     let player:Direction = Direction::Right;
     match player {
@@ -45,10 +51,22 @@ fn nacionalidade_carro(car:CarType){
         CarType::Renault => println!("O carro é frances!"),
     }
 }
+
+fn pagamento_exemplo(){
+    let pessoa_pagamento: Pagamento = Pagamento::CreditoCartao(false, 100f32);
+    match pessoa_pagamento {
+        Pagamento::Dinheiro(f) => println!("A pessoa pagou em Dinheiro {} reais!",f),
+        Pagamento::CreditoCartao(true, x) => println!("A pessoa pagou em Cartão de credito {}!", x),
+        Pagamento::CreditoCartao(false, _) => println!("O pagamento em Cartão de credito não funcionou!"),
+        _ => println!("Opção invalida!"),
+    }
+}
+
 fn main() {
     aula_tupla();
     enums();
     nacionalidade_carro(CarType::Fiat);
     nacionalidade_carro(CarType::Ford);
     nacionalidade_carro(CarType::Renault);
+    pagamento_exemplo();
 }
